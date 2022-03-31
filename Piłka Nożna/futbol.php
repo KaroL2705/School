8a32f13eed5d231c0f3db9288f51cd049d52a99f
KaroL2705 <swierczkarol27@gmail.com>
2022-03-31 13:29:22 +0200

    egzamin

---
+++ Piłka Nożna/futbol.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="styl.css">
+    <title>Rozgrywki futbolowe</title>
+</head>
+<body>
+    <div class="banner">
+        <h2>Światowe rozgrywki piłkarskie</h2>
+        <img src="obraz1.jpg" alt="boisko">
+    </div>
+
+    <div class="mecze">
+
+        <div class="block">
+
+        </div>
+        <?php
+
+        ?>
+    </div>
+
+    <div class="main">
+        <h2>Reprezentacja Polski</h2>
+    </div>
+
+    <div class="left">
+        <p>Podaj pozycję zawodników (1-bramkarze, 2-obrońcy, 3-pomocnicy,
+            4-napastnicy):</p>
+        <form action="POST">
+            <input type="number">
+            <input type="submit" value="Sprwadź">
+        </form>
+
+        <ul>
+            <?php ?>
+        </ul>
+
+    </div>
+
+    <div class="right">
+        <img src="zad1.png" alt="piłkarz">
+    </div>
+</body>
+</html>
